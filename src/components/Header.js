22c178a27a82a5b0441a6cef0775fe8815fb20ec
emmanuelojiji <emmanuelojiji@hotmail.com>
2025-06-19 21:25:59 +0100

    feat: mobile nav implemented + transition

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,33 @@
 import "./Header.scss";
 import logo from "../assets/amg-logo.svg";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+  const [isPopupActive, setIsPopupActive] = useState(false);
+
+  const handleMobileNav = () => {
+    if (!isMobileNavOpen) {
+      setIsMobileNavOpen(true);
+
+      setTimeout(() => {
+        setIsPopupActive(true);
+      }, 50);
+    } else {
+      setIsPopupActive(false);
+
+      setTimeout(() => {
+        setIsMobileNavOpen(false);
+      }, 500);
+    }
+  };
   return (
     <div className="Header">
       <div className="header-left">
         <img src={logo} className="logo-amg" alt="Arrowhead Music Group Logo" />
 
-        <nav>
+        <nav className="desktop-nav">
           <Link to="/">Home</Link>
           <Link to="/roster">Roster</Link>
           <Link to="/about">About</Link>
@@ -16,7 +35,28 @@ const Header = () => {
         </nav>
       </div>
 
-      <Link to="/theriseprogram" className="header-right-link bold">The Rise Program</Link>
+      <p className="mobile-nav-icon" onClick={() => handleMobileNav()}>
+        Open
+      </p>
+
+      <Link to="/theriseprogram" className="header-right-link bold desktop-nav">
+        The Rise Program
+      </Link>
+
+      {isMobileNavOpen && (
+        <nav
+          className={`mobile-nav text-xl ${isPopupActive ? "popup" : ""}`}
+          onClick={() => handleMobileNav()}
+        >
+          <Link to="/">Home</Link>
+          <Link to="/roster">Roster</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/theriseprogram" className="header-right-link bold">
+            The Rise Program
+          </Link>
+        </nav>
+      )}
     </div>
   );
 };
